f63372b23c7ec1bc0ed10766e06afbc5aed18cef
BVM_3 <BVM_3@DESKTOP-5EOVG6K>
2022-02-01 17:55:28 +0530

    File Added into master code base

---
+++ mastergstweb/core/src/main/java/com/mastergst/core/common/AccountConstants.java
@@ -0,0 +1,85 @@
+package com.mastergst.core.common;
+
+public final class AccountConstants {
+
+	public static final String VERSION = "v1.0";
+	
+	
+	public static final String DR = "Dr";
+	public static final String CR = "Cr";
+	public static final String TO = "To";
+	public static final String BY = "By";
+	
+	public static final String OUTPUT_IGST = "Output IGST";
+	public static final String OUTPUT_CGST = "Output CGST";
+	public static final String OUTPUT_SGST = "Output SGST";
+	public static final String OUTPUT_CESS = "Output CESS";
+	
+	public static final String INPUT_IGST = "Input IGST";
+	public static final String INPUT_CGST = "Input CGST";
+	public static final String INPUT_SGST = "Input SGST";
+	public static final String INPUT_CESS = "Input CESS";
+	
+	public static final String OUTPUT_IGST_RCM = "Output IGST RCM";
+	public static final String OUTPUT_CGST_RCM = "Output CGST RCM";
+	public static final String OUTPUT_SGST_RCM = "Output SGST RCM";
+	public static final String OUTPUT_CESS_RCM = "Output CESS RCM";
+	
+	public static final String INPUT_IGST_RCM = "Input IGST RCM";
+	public static final String INPUT_CGST_RCM = "Input CGST RCM";
+	public static final String INPUT_SGST_RCM = "Input SGST RCM";
+	public static final String INPUT_CESS_RCM = "Input CESS RCM";
+	
+	public static final String TAX_ON_ADVANCE = "Tax on Advance";
+	public static final String TAX_ON_ADVANCE_RCM = "Tax on Advance RCM";
+	
+	public static final String TCS_PAYABLE = "TCS Payable";
+	public static final String TCS_RECEIVABLE = "TCS Receivable";
+	public static final String TDS_PAYABLE = "TDS Payable";
+	public static final String TDS_RECEIVABLE = "TDS Receivable";
+	
+	public static final String  IGST_REFUND_RECEIVABLE ="IGST Refund Receivable";
+	public static final String  CESS_REFUND_RECEIVABLE ="Cess Refund Receivable";
+	
+	public static final String  OTHER_DEBTORS = "Other Debtors";
+	public static final String  OTHER_CREDITORS = "Other Creditors";
+	
+	public static final String  SUNDRY_DEBTORS = "Sundry Debtors";
+	public static final String  SUNDRY_CREDITORS = "Sundry Creditors";
+	
+	public static final String  ADVANCE_FROM = "Advance From";
+	
+	public static final String  WHITE_SPACE = " ";
+	public static final String  AND = "&";
+	public static final String  OR = "|";
+	
+	public static final String  CASH = "Cash";
+	public static final String  BANK = "Bank";
+	public static final String BANK_ACCOUNTS = "Bank accounts";
+	public static final String  SALES = "Sales";
+	public static final String  PURCHASES = "Purchases";
+	public static final String SALES_DEFAULT_PATH = "Income/Direct Income/Sales";
+	public static final String BANK_DEFAULT_PATH = "Assets/Current Assets/Bank accounts";
+	public static final String CASH_DEFAULT_PATH = "Assets/Current Assets/Cash in hand";
+	
+	public static final String SALES_EXEMPT = "Sales-Exempt";
+	public static final String PURCHASE_EXEMPT = "Purchase-Exempt";
+
+	public static final String OTHER_EXPENSES = "Other expenses";
+	public static final String HO_ISD = "HO-ISD";
+	public static final String ISD_INELIGIBLE = "Input Ineligible ISD";
+	public static final String INPUT_ITC_REVERSAL = "Input ITC Reversal";
+	
+	public static final String PAN_A = "Association of Persons (AOP)";
+	public static final String PAN_B = "Body of Indiviuals (BOI)";
+	public static final String PAN_C = "Company";
+	public static final String PAN_F = "Firm";
+	public static final String PAN_G = "Governament Agency";
+	public static final String PAN_H = "Hindu Undivided Family (HUF)";
+	public static final String PAN_J = "Artificial Judicial Person";
+	public static final String PAN_L = "Local Authority";
+	public static final String PAN_P = "Person / Indiviual";
+	public static final String PAN_T = "Trusts";
+	
+	
+}
